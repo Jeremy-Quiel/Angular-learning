a22d3227f2228b237b8b0b403c8ac8b21ff57c20
Jeremy <ing.jeremy.quiel@gmail.com>
2026-08-13 19:32:45 -0600

    fix: move counter component right

--- bases/src/app/app.routes.ts
+++ bases/src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
-import { counterComponent } from './pages/counter.component';
+import { counterComponent } from './pages/counter/counter.component';
+import { heroComponent } from './pages/hero/hero.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: counterComponent
+  },
+
+  {
+    path: 'hero',
+    component: heroComponent
   }
 ];
