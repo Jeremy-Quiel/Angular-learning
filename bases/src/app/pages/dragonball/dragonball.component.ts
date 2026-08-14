6946b4f57fc03ad8c739c2c81504e5bedf6f0809
Jeremy <ing.jeremy.quiel@gmail.com>
2026-08-14 12:49:11 -0600

    feat(dragonball): add logic to dragonball page

--- bases/src/app/pages/dragonball/dragonball.component.ts
+++ bases/src/app/pages/dragonball/dragonball.component.ts
@@ -11,7 +11,9 @@ interface Character {
 })
 export class dragonballComponent{
   name = signal("Gohan")
-  power = signal("50")
+  power = signal(50)
+
+  error = signal(false)
 
   characters = signal<Character[]>([
     {id:1, name: "Goku", power: 9001},
@@ -19,4 +21,18 @@ export class dragonballComponent{
     {id:3, name: "Piccolo", power: 5000},
     {id:4, name: "Yamcha", power: 500}
   ])
+
+  addCharacter(){
+    if (this.name() == '' || this.power() <= 0) {
+      this.error.set(true)
+      return;
+    }
+
+    this.error.set(false)
+
+    this.characters.update(c => [...c, {id: this.characters().length + 1, name: this.name(), power: this.power() }])
+
+    this.name.set("")
+    this.power.set(0)
+  }
 }
