5a731fce21a2edd4900f9930a005a3cc0790db85
Jeremy <ing.jeremy.quiel@gmail.com>
2026-08-13 19:48:50 -0600

    style: change bold dd class (css)

--- bases/src/app/pages/hero/hero.component.ts
+++ bases/src/app/pages/hero/hero.component.ts
@@ -1,7 +1,12 @@
-import { Component, signal } from "@angular/core";
+import { Component, computed, signal } from "@angular/core";
 
 @Component({
-  templateUrl: "./hero.component.html"
+  templateUrl: "./hero.component.html",
+  styles:   `
+  dd {
+    font-weight: 900;
+  }
+  `
 })
 export class heroComponent{
   name = signal("Iron-Man");
@@ -25,8 +30,8 @@ export class heroComponent{
     this.age.set(age)
   }
 
-  capitalize(){
+  capitalize = computed(() => {
     return this.name().toUpperCase()
-  }
+  });
 
 }
