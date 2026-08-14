20ad73632725af37c1005f1d63c9f6cca6b46e1c
Jeremy <ing.jeremy.quiel@gmail.com>
2026-08-13 20:20:54 -0600

    feat: add 404 page (not found)

--- bases/src/app/app.routes.ts
+++ bases/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { counterComponent } from './pages/counter/counter.component';
 import { heroComponent } from './pages/hero/hero.component';
+import { _404Component } from './pages/404/404.component';
 
 export const routes: Routes = [
   {
@@ -11,5 +12,10 @@ export const routes: Routes = [
   {
     path: 'hero',
     component: heroComponent
+  },
+
+  {
+    path: '**',
+    component: _404Component
   }
 ];
