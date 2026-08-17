5c97f02172a54267ed124f8ff9ca30ea763a0ff2
Jeremy <ing.jeremy.quiel@gmail.com>
2026-08-17 13:18:44 -0600

    refactor: use alias to import environments

--- gifs-app/src/app/gifs/components/dashboard/side-menu/side-menu.component.ts
+++ gifs-app/src/app/gifs/components/dashboard/side-menu/side-menu.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { SideMenuHeaderComponent } from '../side-menu-header/side-menu-header.component';
 import { SideMenuOptionsComponent } from '../side-menu-options/side-menu-options.component';
-import { environment } from '../../../../../environments/environment.development';
+import { environment } from '@environments/environment';
+
 
 @Component({
   selector: 'gifs-side-menu',
