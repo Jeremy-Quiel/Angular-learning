c89cbdfb42b7447303ecf6191d77fd75ff1939dd
Jeremy <ing.jeremy.quiel@gmail.com>
2026-08-16 21:28:09 -0600

    feat: add routerLink to dashboard

--- gifs-app/src/app/gifs/components/dashboard/side-menu-options/side-menu-options.component.ts
+++ gifs-app/src/app/gifs/components/dashboard/side-menu-options/side-menu-options.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
+import { MenuOptions } from '../../../interfaces/dashboard/MenuOptions.interface';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu-options.component.html'
 })
-export class SideMenuOptionsComponent {}
+export class SideMenuOptionsComponent {
+  menuOptions: MenuOptions[] = [
+    {
+      label: 'Trending',
+      sublabel: 'Gifs Populares',
+      route: '/dashboard/trending',
+      icon: 'fa-solid fa-trophy'
+    },
+
+    {
+      label: 'Search',
+      sublabel: 'Search Gifs',
+      route: '/dashboard/search',
+      icon: 'fa-solid fa-magnifying-glass'
+    }
+  ]
+}
