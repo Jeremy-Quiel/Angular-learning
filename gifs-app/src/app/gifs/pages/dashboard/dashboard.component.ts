48a5aeba3d4d59981849b4a6b2bf21a934d3b795
Jeremy <ing.jeremy.quiel@gmail.com>
2026-08-16 20:19:10 -0600

    refactor: separate menu-side-bar-options/header by components

--- gifs-app/src/app/gifs/pages/dashboard/dashboard.component.ts
+++ gifs-app/src/app/gifs/pages/dashboard/dashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from "@angular/router";
+import { SideMenuHeaderComponent } from '../../components/dashboard/side-menu-header/side-menu-header.component';
+import { SideMenuOptionsComponent } from '../../components/dashboard/side-menu-options/side-menu-options.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideMenuHeaderComponent, SideMenuOptionsComponent],
   templateUrl: './dashboard.component.html',
 })
 export default class DashboardComponent {}
