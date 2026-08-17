a6e2618fb280c53aa2afdb785b0347995a47f51c
Jeremy <ing.jeremy.quiel@gmail.com>
2026-08-17 13:11:52 -0600

    feat: use environment variables in app

--- gifs-app/src/app/gifs/components/dashboard/side-menu/side-menu.component.ts
+++ gifs-app/src/app/gifs/components/dashboard/side-menu/side-menu.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { SideMenuHeaderComponent } from '../side-menu-header/side-menu-header.component';
 import { SideMenuOptionsComponent } from '../side-menu-options/side-menu-options.component';
+import { environment } from '../../../../../environments/environment.development';
 
 @Component({
   selector: 'gifs-side-menu',
   imports: [SideMenuHeaderComponent, SideMenuOptionsComponent],
   templateUrl: './side-menu.component.html',
 })
-export class SideMenuComponent {}
+export class SideMenuComponent {
+  env = environment
+}
---
+++ gifs-app/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  companyName: 'GIFS',
+  companySecName: 'APP',
+  companySlogan: 'Found the best Gifs'
+};
