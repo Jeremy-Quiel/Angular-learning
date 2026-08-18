d79b5714906704f3086b3062aaf068598e9fe88e
Jeremy <ing.jeremy.quiel@gmail.com>
2026-08-17 18:48:34 -0600

    feat: display gifs on screen

--- gifs-app/src/app/gifs/components/trending/gifs-list-item.component/gifs-list-item.component.ts
+++ gifs-app/src/app/gifs/components/trending/gifs-list-item.component/gifs-list-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, input } from '@angular/core';
+import { Gif } from 'src/app/gifs/interfaces/giphy/Gif.interface';
 
 @Component({
   selector: 'gifs-list-item',
@@ -6,5 +7,5 @@ import { Component, input } from '@angular/core';
   templateUrl: './gifs-list-item.component.html',
 })
 export class GifsListItemComponent {
-  src = input.required<string>()
+  gif = input.required<Gif>()
 }
--- gifs-app/src/app/gifs/components/trending/gifs-list.component/gifs-list.component.ts
+++ gifs-app/src/app/gifs/components/trending/gifs-list.component/gifs-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, signal, WritableSignal } from '@angular/core';
+import { Component, inject} from '@angular/core';
 import { GifsListItemComponent } from "../gifs-list-item.component/gifs-list-item.component";
+import { GifsService } from 'src/app/gifs/services/gifs.service';
 
 @Component({
   selector: 'gifs-list',
@@ -7,15 +8,9 @@ import { GifsListItemComponent } from "../gifs-list-item.component/gifs-list-ite
   templateUrl: './gifs-list.component.html',
 })
 export class GifsListComponent {
+ 
+  gifsService = inject(GifsService)
 
-  srcs: WritableSignal<string[]> = signal<string[]>(['https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg'])
-
-  constructor(){
-    for(let i = 1; i <= 11; i ++){
-      let src: string = 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image' + '-' + i +'.jpg'
-      this.srcs.update(c => [...c, src])
-    }
-  }
-
+  gifs = this.gifsService.trendingGifs
 
 }
